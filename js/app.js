e03435167843e8e6e2de8f9b90942e2aa3ed1a2a
Rob Lum <lum.robert@gmail.com>
2016-02-16 09:58:45 -0500

    feat(index): finished basic less examples

--- js/app.js
+++ js/app.js
@@ -1,34 +1,16 @@
 var Reveal = require('reveal');
+require('../lib/head.min.js');
+
+// compile with less-loader
 require('../styles/index.less');
 
-document.addEventListener("DOMContentLoaded", init, false);
-
-function init() {
-    var cblocks = document.querySelectorAll("pre code");
-
-    if(cblocks.length) {
-        for(var i=0, len=cblocks.length; i<len; i++) {
-            var dom = cblocks[i];
-            var html = dom.innerHTML;
-            html = html.replace(/</g,"&lt;").replace(/>/g,"&gt;");
-            dom.innerHTML = html;
-        }
-    }
-
-    Reveal.initialize({
-        controls: true,
-        progress: true,
-        history: true,
-        center: true,
-        transition: 'slide'
-    });
-
-}
-
-// Reveal.initialize({
-//     controls: true,
-//     progress: true,
-//     history: true,
-//     center: true,
-//     transition: 'slide'
-// });
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    transition: 'slide',
+    dependencies: [
+        { src: './lib/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+    ]
+});
