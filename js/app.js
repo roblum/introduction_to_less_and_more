a6d21fc14748ed1102d266ebe22d40ab9f20a663
Rob Lum <lum.robert@gmail.com>
2016-02-15 22:37:10 -0500

    refactor(index): intro less first; using reveal code init script temporarily

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 var Reveal = require('reveal');
 require('../styles/index.less');
 
-Reveal.initialize({
-    controls: true,
-    progress: true,
-    history: true,
-    center: true,
-    transition: 'slide',
-    dependencies: [
-        { src: 'lib/highlight.js', aync: true, callback: function() { hljs.initHighlightingOnLoad(); }}
-    ]
-});
+document.addEventListener("DOMContentLoaded", init, false);
+
+function init() {
+    var cblocks = document.querySelectorAll("pre code");
+
+    if(cblocks.length) {
+        for(var i=0, len=cblocks.length; i<len; i++) {
+            var dom = cblocks[i];
+            var html = dom.innerHTML;
+            html = html.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+            dom.innerHTML = html;
+        }
+    }
+
+    Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+        transition: 'slide'
+    });
+
+}
+
+// Reveal.initialize({
+//     controls: true,
+//     progress: true,
+//     history: true,
+//     center: true,
+//     transition: 'slide'
+// });
